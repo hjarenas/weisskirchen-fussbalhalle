959415b1bec5bbc75ab0dab432533f4e6fe5dfb2
hjarenas <hector.it.dev@gmail.com>
2023-10-21 22:07:47 +0200

    add editorconfig

--- src/views/PastMatches.tsx
+++ src/views/PastMatches.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PastMatches: React.FC = () => {
-    return <div>Past Matches Page</div>;
+  return <div>Past Matches Page</div>;
 };
 
 export default PastMatches;
--- src/views/RecurringPlayers.tsx
+++ src/views/RecurringPlayers.tsx
@@ -4,24 +4,24 @@ import { firestoreDb } from '../firebase';
 import { Button } from '@mui/material';
 
 const addPlayer = async () => {
-    console.log("Adding team... ");
-    try {
-        const docRef = await addDoc(collection(firestoreDb, "recurring-players"), {
-            name: "Example Football Team"
-        });
-        console.log("Document written with ID: ", docRef.id);
-    } catch (error) {
-        console.error("Error adding document: ", error);
-    }
+  console.log("Adding team... ");
+  try {
+    const docRef = await addDoc(collection(firestoreDb, "recurring-players"), {
+      name: "Example Football Team"
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
 };
 const RecurringPlayers: React.FC = () => {
-    return (
-        <div className="App">
-            <Button variant="contained" color="primary" onClick={addPlayer}>
-                Add Team
-            </Button>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary" onClick={addPlayer}>
+        Add Team
+      </Button>
+    </div>
+  );
 };
 
 export default RecurringPlayers;
