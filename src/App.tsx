2210f64f33ce98d53ee2ef143dec5a9404e06d9d
hjarenas <hector.it.dev@gmail.com>
2023-10-23 23:03:10 +0200

    styling of layout + support small screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CreateNewMatchView from './views/CreateMatch';
 import RecurringPlayersView from './views/RecurringPlayersView';
 import PastMatchesView from './views/PastMatches';
 import Home from './views/Home';
-import { AppBar, CssBaseline, Drawer, Toolbar, Typography } from '@mui/material';
+import { AppBar, CssBaseline, Drawer, Hidden, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import Sidebar from './components/Sidebar/Sidebar';
+import './App.css';
+
+
 
 const App: React.FC = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
   return (
     <BrowserRouter>
       <CssBaseline />
-      <AppBar position="fixed" style={{ zIndex: 1301 }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+        }}>
         <Toolbar>
+          {/* Other toolbar items */}
+          <Hidden mdUp>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="end"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
           <Typography variant="h6" noWrap>
             Matches Tracker
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer
-        variant="permanent"
-        style={{ width: 240 }}
-        sx={{
-          width: 240,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
-        }}
-      >
-        <Toolbar />
-        <Sidebar />
-      </Drawer>
-      <main style={{ marginLeft: 250, marginTop: 10 }}>
-        <Toolbar />
+      {/* Permanent drawer for larger screens */}
+      <Hidden mdDown>
+        <Drawer
+          variant="permanent"
+          open
+        >
+          <div style={{ marginTop: '64px' }}>
+            <Sidebar />
+          </div>
+        </Drawer>
+      </Hidden>
+      {/* Temporary drawer for smaller screens */}
+      <Hidden mdUp>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+        >
+          <Sidebar />
+        </Drawer>
+      </Hidden>
+
+      <main className='app-main-content'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/recurring-players" element={<RecurringPlayersView />} />
